f2ea46094d8294c77dd86ff70b99e64d91c3c1a1
heufel <toborobo9000@gmail.com>
2025-06-03 04:43:48 -0700

    Finally got things working (and got some optimization too!)

--- progress_bar.py
+++ progress_bar.py
@@ -1,5 +1,5 @@
-def progress_bar(completion: float, length: int, suffix=''):
+def progress_bar(completed: float, total: float, length: int, suffix=''):
     end = '\r'
-    if completion == 1:
+    if total == 1:
         end = '\n'
-    print(f'{completion: .2%} |{''.join([chr(0x2588) if (i/length < completion) else ' ' for i in range(length)])}|{suffix}', end=end)
+    print(f'{(completed / total): .2%} - {completed}/{total} |{''.join([chr(0x2588) if (i/length < completed/total) else ' ' for i in range(length)])}|{suffix}', end=end)
